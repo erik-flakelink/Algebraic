8ae6a96d4c5b387663bb1b2154772f719dda1eff
Erik <122167559+eriktherdev@users.noreply.github.com>
2023-02-24 13:32:27 -0500

    Update algfunc.h

--- algfunc.h
+++ algfunc.h
@@ -8,6 +8,6 @@ int sub(); //Subtraction of two or more integers.
 int mtp(); //Multiplication of two or more integers.
 int exp(); //Exponentation of a base and a power.
 int ftd(); //Floor Division of two integers.
-int sqrt(); //Square Root of an integer.
+int sqr(); //Square Root of an integer.
 int pi(); //Evaluates the pi. Takes in an integer value (int) and returns 3.141592.
 int pop(); //Power of Powers: Takes in a Base and two or more powers.
