16101b9d92e97ed880a883bd10cb82debbb9bdd9
Erik <122167559+erik-cobalt@users.noreply.github.com>
2023-02-24 13:14:54 -0500

    Create algfunc.c

---
+++ algfunc.c
@@ -0,0 +1,15 @@
+//Main Function
+int algebraic();
+  printf("ALGEBRAIC [R]");
+  printf("Created by Erik-Cobalt");
+
+//Sub Functions
+int add(); //Addition of two or more integers
+int div(); //Division of two integers.
+int sub(); //Subtraction of two or more integers.
+int mtp(); //Multiplication of two or more integers.
+int exp(); //Exponentation of a base and a power.
+int ftd(); //Floor Division of two integers.
+int sqrt(); //Square Root of an integer.
+int pi(); //Evaluates the pi. Takes in an integer value (int) and returns 3.141592.
+int pop(); //Power of Powers: Takes in a Base and two or more powers.
